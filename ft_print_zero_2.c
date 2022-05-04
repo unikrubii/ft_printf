abecba60bb5207442b3b1cd92d46336f4f93b097
unikrubii <gamez.zeta@gmail.com>
2022-05-04 10:29:57 +0700

    fix pointer

--- ft_print_zero_2.c
+++ ft_print_zero_2.c
@@ -6,7 +6,7 @@
 /*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 15:11:48 by sthitiku          #+#    #+#             */
-/*   Updated: 2022/05/01 02:35:19 by sthitiku         ###   ########.fr       */
+/*   Updated: 2022/05/04 10:27:08 by sthitiku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,12 +48,11 @@ void	z_int(int width, t_f *ar, char sp_c)
 	ft_putnbr(num, ar);
 }
 
-void	z_pointer(size_t width, t_f *ar, char sp_c)
+int	z_width(size_t base, size_t width)
 {
-	size_t	i;
-	size_t	base;
-
-	base = (size_t)va_arg(ar->arg, size_t);
+	size_t len;
+	
+	len = d_u(base, 16, 0);
 	if (!base)
 	{
 		if (width >= 3)
@@ -66,8 +65,23 @@ void	z_pointer(size_t width, t_f *ar, char sp_c)
 		if (width >= 14)
 			width -= 14;
 		else
-			width = 0;
+		{
+			if (width > len + 2)
+				width = width - (len + 2);
+			else
+				width = 0;
+		}
 	}
+	return (width);
+}
+
+void	z_pointer(size_t width, t_f *ar, char sp_c)
+{
+	size_t	i;
+	size_t	base;
+
+	base = (size_t)va_arg(ar->arg, size_t);
+	width = z_width(base, width);
 	i = 0;
 	while (i < width)
 	{
